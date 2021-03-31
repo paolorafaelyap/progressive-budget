63297f1af0db751b2ddd85ba3b9dbe825921d21c
paolorafaelyap <paolorafaelyap@gmail.com>
2021-03-30 18:41:31 -0700

    updated service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 "use strict"
 
-const FILES_TO_CACHE = [
+const CACHE_FILES = [
     `/db.js`,
     `/index.js`,
     `/index.css`,
@@ -14,7 +14,7 @@ const RUNTIME_CACHE =`runtime-cache`;
 self.addEventListener(`install`, event => {
     event.waitUntil(
         caches.open(STATIC_CACHE)
-            .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(cache => cache.addAll(CACHE_FILES))
             .then(() => self.skipWaiting())
     );
 });
@@ -35,3 +35,42 @@ self.addEventListener(`activate`, event => {
     );
 });
 
+self.addEventListener(`fetch`, event => {
+    if (
+        event.request.method !== `GET` ||
+        !event.request.url.startsWith(self.location.origin)
+    ) {
+        event.respondWith(fetch(event.request));
+        return;
+    }
+
+    if (event.request.url.includes(`/api/transaction`)) {
+        event.respondWith(
+            caches.open(RUNTIME_CACHE).then(cache =>
+                fetch(event.request)
+                    .then(response => {
+                        cache.put(event.request, response.clone());
+                        return response;
+                    })
+                    .catch(()=> caches.match(event.request))
+            )
+        );
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+
+            return caches
+                .open(RUNTIME_CACHE)
+                .then(cache =>
+                    fetch(event.request).then(response =>
+                        cache.put(event.request, response.clone()).then(() => response)
+                        )
+                    );
+        })
+    );
+});

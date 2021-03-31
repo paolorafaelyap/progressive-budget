053ac4ba664479e92fbd509e674a237b7bfd1569
paolorafaelyap <paolorafaelyap@gmail.com>
2021-03-30 17:34:09 -0700

    added function

--- public/db.js
+++ public/db.js
@@ -13,11 +13,46 @@ request.onupgradeneeded = event => {
 };
 
 // if app is online, read from db
+//success and error handlers
 request.onsuccess = event => {
     console.log(`${event.type}`);
     if (navigator.onLine) {
         checkDatabase();
     }
-}
+};
 
+request.onerror = event => console.error(event);
+
+//function for checking database to be added here
+function checkDatabase() {
+    const db = request.result;
+    // opens transaction for pending db
+    let transaction = db.transaction([pendingObjectName], `readwrite`);
+    // accesses pending object store
+    let store = transaction.objectStore(pendingObjectName);
+    // gets all records from object store
+    const allRecords = store.allRecords();
+
+    allRecords.onsuccess = () => {
+        if(allRecords.result.length > 0) {
+
+            fetch(`/api/transaction/bulk`, {
+                method: `POST`,
+                body: JSON.stringify(allRecords.result),
+                headers: {
+                    Accept: `application/json, text/plain, */*`,
+                    "Content-Type": `application/json`
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                transaction = db.transaction([pendingObjectName], `readwrite`);
+                store = transaction.objectStore(pendingObjectName);
+                //this will clear store items
+                store.clear();
+            });
+        }
+
+    };
+}
 
